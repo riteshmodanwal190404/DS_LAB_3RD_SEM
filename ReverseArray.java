4c0865822354805d8a58521ae1da0cbe5eab2287
Ritesh Modanwal <riteshmodanwal1904@gmail.com>
2025-12-10 19:57:07 +0530

    Add files via upload

---
+++ ReverseArray.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Reverse logic
+        for (int i = 0, j = n - 1; i < j; i++, j--) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        System.out.println("Reversed Array:");
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+}
