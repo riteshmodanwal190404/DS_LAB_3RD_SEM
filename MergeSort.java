18f2a96078d2fcec3e872df37e00055e03de7ccc
Ritesh Modanwal <riteshmodanwal1904@gmail.com>
2025-12-11 13:03:22 +0530

    Add files via upload

---
+++ MergeSort.java
@@ -0,0 +1,64 @@
+public class MergeSort {
+
+    public static void merge(int[] arr, int l, int m, int r) {
+
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        // Copy data to temp arrays
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[l + i];
+
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[m + 1 + j];
+
+        int i = 0, j = 0, k = l;
+
+        // Merge the two halves
+        while (i < n1 && j < n2) {
+
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+
+            k++;
+        }
+
+        // Copy remaining elements
+        while (i < n1)
+            arr[k++] = L[i++];
+
+        while (j < n2)
+            arr[k++] = R[j++];
+    }
+
+    public static void mergeSort(int[] arr, int l, int r) {
+
+        if (l < r) {
+            int m = (l + r) / 2;
+
+            mergeSort(arr, l, m);       // Left half
+            mergeSort(arr, m + 1, r);   // Right half
+
+            merge(arr, l, m, r);        // Merge halves
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {12, 11, 13, 5, 6, 7};
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.print("Sorted Array: ");
+        for (int x : arr)
+            System.out.print(x + " ");
+    }
+}
